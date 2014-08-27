209b952934326c7caedbde71e86f8dfbd7f81b1f
Vitaly <vitaly.kiktiov@gmail.com>
2014-08-27 11:35:45 +0300

    Distributor controller

--- controllers/DistributorController.php
+++ controllers/DistributorController.php
@@ -17,6 +17,9 @@
      */
     class DistributorController extends Controller
     {
+        /**
+         * @return array
+         */
         public function behaviors()
         {
             return [
@@ -67,20 +70,26 @@
          */
         public function actionCreate()
         {
+            //prepare model
             $model = new Distributor();
-
+            //get post request into variable
             $request = Yii::$app->request->post();
-
+            //if request is not empty
             if (!empty($request)) {
+                //load data into model
                 $model->load($request);
+                //call distributor registering logic and if it returns true
                 if ($model->registerDistributor()) {
+                    //redirect browser to view action
                     return $this->redirect(['view', 'id' => $model->id]);
                 } else {
+                    //if distributor registering returned false - re-render 'create' page using current model
                     return $this->render('create', [
                         'model' => $model,
                     ]);
                 }
             } else {
+                // if request is empty - render 'create' screen using blank model
                 return $this->render('create', [
                     'model' => $model,
                 ]);
@@ -95,21 +104,29 @@
          */
         public function actionUpdate($id)
         {
+            //find model using given id
             $model = $this->findModel($id);
+            //get post request into variable
             $request = Yii::$app->request->post();
-
+            //if request is not empty
             if (!empty($request)) {
+                //load data into model
                 $model->load($request);
-                if ($model->save()) {
-                    $model->saveCountry();
-
+                //call distributor updating logic logic and if it returns true
+                if ($model->updateDistributor()) {
+                    //redirect browser to view action
                     return $this->redirect(['view', 'id' => $model->id]);
                 } else {
-                    //TODO Handle error while saving
+                    //if distributor updating returned false - re-render 'update' page using current model
+                    return $this->render('update', [
+                        'model' => $model,
+                    ]);
                 }
             } else {
+                // if request is empty - initialize country_id field
                 $model->country_id = $model->getCountryId();
 
+                //render 'update' screen using blank model
                 return $this->render('update', [
                     'model' => $model,
                 ]);
@@ -145,42 +162,88 @@
             }
         }
 
-        public function actionList()
+        /**
+         * Returns json array with distributors
+         *
+         * @param null $fields gives capability to specify required fields. If null will return default set
+         * @return string
+         */
+        public function actionList($fields = null)
         {
+            //get all distributors into array
             $distributors = Distributor::find()->all();
+            //initialize result variuable
             $result = [];
-            /**@var Distributor $distributor */
-            foreach ($distributors as $distributor) {
-                $d['id'] = $distributor->id;
-                $d['title'] = $distributor->title;
-                $d['email'] = $distributor->email;;
-                $d['country'] = $distributor->countryName;
-                $result[] = $d;
+            //check if fields are specified
+            if ($fields) {
+                //get array with needed fields from specified string
+                $specField = explode(",", $fields);
+                /** @var $distributor Distributor */
+                foreach ($distributors as $distributor) {
+                    $d = null;
+                    //for each field if it's not empty
+                    foreach ($specField as $field) {
+                        if ($field != "") {
+                            //get field from distributor and set same field for temp variable
+                            $d[$field] = $distributor[$field];
+                        }
+                    }
+                    //if temp variable is set
+                    if (isset($d)) {
+                        //add it to the result array
+                        $result[] = $d;
+                    }
+                }
+            } else {
+                //if fields are not specified return default set
+                /**@var Distributor $distributor */
+                foreach ($distributors as $distributor) {
+                    $d['id'] = $distributor->id;
+                    $d['title'] = $distributor->title;
+                    $d['email'] = $distributor->email;;
+                    $d['country'] = $distributor->countryName; //TODO return full country object and get it's name after
+                    $result[] = $d;
+                }
             }
-
+            //return result array in json format
             echo(Json::encode($result));
         }
 
+        /**
+         * Get country specified in "dependent dropdown"
+         * Returns json encoded list of distributors assigned to the specified country
+         *
+         * @param depdrop_parents
+         * @return string
+         */
         public function actionDynamic()
         {
+            //check if value in parent dropdown has been specified
             if (isset($_POST['depdrop_parents'])) {
+                //get list of parent dropdowns
                 $parents = $_POST['depdrop_parents'];
                 if ($parents != null) {
+                    //get first value of array with parameters from parent dropdown
                     $countryId = $parents[0];
+                    //find a country by the id
                     $country = Country::findOne(['id_countries' => $countryId]);
+                    //get list of distributors assigned to found country
                     $distributors = $country->distributors;
                     $out = [];
                     $res = [];
+                    //prepare array with found distributors
                     foreach ($distributors as $d) {
                         $res['id'] = $d->id;
                         $res['name'] = $d->title;
                         $out[] = $res;
                     }
+                    //return json array with list of distributors
                     echo Json::encode(['output' => $out, 'selected' => '']);
-
-                    return;
                 }
+            } else {
+                //if country is not specified return empty list
+                echo Json::encode(['output' => '', 'selected' => '']);
             }
-            echo Json::encode(['output' => '', 'selected' => '']);
+
         }
     }
