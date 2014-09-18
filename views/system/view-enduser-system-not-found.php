16056585e87c9df39e3e906bf9b0c0503adb5bfd
Vitaly <vitaly.kiktiov@gmail.com>
2014-09-18 16:35:48 +0300

    #41 Error "Not Found (#404)" when you push view System (EndUser connection)

---
+++ views/system/view-enduser-system-not-found.php
@@ -0,0 +1,18 @@
+<?php
+
+    /* @var $this yii\web\View */
+
+    $this->title = Yii::t('app', 'System Details');
+    $this->params['breadcrumbs'][] = $this->title;
+?>
+
+<div class="systems-view">
+    <div class="jumbotron">
+        <h1><?= Yii::t('app', 'System is not assigned yet') ?></h1>
+
+        <p>
+            <?= Yii::t('app', 'This page will display all system-related data once it will be assigned. Contact your distributor and check for updates') ?>
+        </p>
+    </div>
+</div>
+
