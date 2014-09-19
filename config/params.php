2a6f5d6c03e1db4381da115407cf80acc95da719
Vitaly <vitaly.kiktiov@gmail.com>
2014-09-19 16:16:11 +0300

    #45 Endymed Support Email + Hint to contact when system is assigned

--- config/params.php
+++ config/params.php
@@ -2,5 +2,6 @@
 
     return [
         'adminEmail' => 'admin@example.com',
-        'version'    => '0.0.0.1'
+        'version'          => '0.0.0.1',
+        'mainSupportEmail' => 'main_endymed_support@mailinator.com'
     ];
