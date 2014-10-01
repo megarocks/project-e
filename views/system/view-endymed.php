d03b8479781c1e9072676846da07cd323e98ce61
Vitaly <vitaly.kiktiov@gmail.com>
2014-10-01 13:19:57 +0300

    #136 Wrong text in 'Next locking date' line after last payment from End-User (Administrator)

--- views/system/view-endymed.php
+++ views/system/view-endymed.php
@@ -18,73 +18,77 @@
     Yii::$app->user->setReturnUrl(Url::to());
 ?>
 
-<div class="systems-view">
-    <p>
-        <?= Html::a(Yii::t('app', 'View All'), ['index'], ['class' => 'btn btn-default']); ?>
-        <?php if (!isset($po)) : ?>
-            <?= Html::a(Yii::t('app', 'Assign to PO'), ['assign', 'system_id' => $model->id], ['class' => 'btn btn-primary']) ?>
-            <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']); ?>
-            <?= Html::a(Yii::t('app', 'Delete'), ['#'], ['class' => 'btn btn-danger delete-button', 'requestLink' => Url::toRoute(['delete', 'id' => $model->id])]); ?>
+<?php if (!is_null($po)) : ?>
+    <div class="systems-view">
+        <p>
+            <?= Html::a(Yii::t('app', 'View All'), ['index'], ['class' => 'btn btn-default']); ?>
+            <?php if ($po->editable) : ?>
+                <?= Html::a(Yii::t('app', 'Unassign from PO'), ['unassign', 'id' => $model->id], ['class' => 'btn btn-danger']); ?>
+            <?php endif; ?>
+            <?php if (($po->ctpl > 0) || ($po->dtpl > 0)) : ?>
+                <?= Html::a(Yii::t('app', 'Add Payment'), ['/payment/create', 'access_token' => $model->access_token], ['class' => 'btn btn-primary']); ?>
+            <?php endif; ?>
+        </p>
 
-        <?php endif; ?>
-        <?php if (isset($po)) :
-            if ($po->editable) :
-                echo Html::a(Yii::t('app', 'Unassign from PO'), ['unassign', 'id' => $model->id], ['class' => 'btn btn-danger']);
-                echo "&nbsp";
-            endif;
-            echo Html::a(Yii::t('app', 'Add Payment'), ['/payment/create', 'access_token' => $model->access_token], ['class' => 'btn btn-primary']);
-        endif; ?>
-    </p>
+        <h3><?= Yii::t('app', 'System Details') ?> </h3>
 
-    <h3><?= Yii::t('app', 'System Details') ?> </h3>
+        <?php
+            $systemDetailsAttributes = [];
+            if ($po->cnpl > 0) :
+                $systemDetailsAttributes = ['sn', 'systemStatus', 'login_code', 'main_unlock_code', 'current_code', 'initialLockingDate', 'nextLockingDate', 'createdAt'];
+            else :
+                $systemDetailsAttributes = ['sn', 'systemStatus', 'login_code', 'main_unlock_code', 'createdAt'];
+            endif; ?>
 
-    <div class="well">
-        <?=
-            PpdDetailView::widget([
-                'model'      => $model,
-                'attributes' => [
-                    'sn',
-                    'systemStatus',
-                    'login_code',
-                    'main_unlock_code',
-                    'current_code',
-                    'initialLockingDate',
-                    'nextLockingDate',
-                    'createdAt',
-                ],
-            ]) ?>
-    </div>
+        <div class="well">
+            <?=
+                PpdDetailView::widget([
+                    'model'      => $model,
+                    'attributes' => $systemDetailsAttributes,
+                ]) ?>
+        </div>
 
-    <?php if (isset($po)): ?>
         <h3><?= Yii::t('app', 'Customer monetary details') ?> </h3>
         <?=
-    PpdDetailView::widget([
-        'model'      => $po,
-        'attributes' => [
-            'po_num',
-            'csp',
-            'cpup',
-            'nop',
-            'cnpl',
-            'ctpl',
-            'cmp',
-        ],
-    ]) ?>
+            PpdDetailView::widget([
+                'model'      => $po,
+                'attributes' => ['po_num', 'csp', 'cpup', 'nop', 'cnpl', 'ctpl', 'cmp',]
+            ]) ?>
 
         <h3><?= Yii::t('app', 'Distributor monetary details') ?> </h3>
         <?=
-        PpdDetailView::widget([
-            'model'      => $po,
-            'attributes' => [
-                'po_num',
-                'dsp',
-                'dpup',
-                'nop',
-                'dnpl',
-                'dtpl',
-                'dmp',
-            ],
-        ]) ?>
-    <?php endif; ?>
+            PpdDetailView::widget([
+                'model'      => $po,
+                'attributes' => ['po_num', 'dsp', 'dpup', 'nop', 'dnpl', 'dtpl', 'dmp']
+            ]) ?>
 
-</div>
+    </div>
+<?php endif; ?>
+
+<?php if (is_null($po)) : ?>
+
+    <div class="systems-view">
+
+        <p>
+            <?= Html::a(Yii::t('app', 'View All'), ['index'], ['class' => 'btn btn-default']); ?>
+            <?= Html::a(Yii::t('app', 'Assign to PO'), ['assign', 'system_id' => $model->id], ['class' => 'btn btn-primary']) ?>
+            <?= Html::a(Yii::t('app', 'Update'), ['update', 'id' => $model->id], ['class' => 'btn btn-primary']); ?>
+            <?= Html::a(Yii::t('app', 'Delete'), ['#'], ['class' => 'btn btn-danger delete-button', 'requestLink' => Url::toRoute(['delete', 'id' => $model->id])]); ?>
+        </p>
+
+        <?php
+            $systemDetailsAttributes = ['sn', 'systemStatus', 'login_code', 'main_unlock_code', 'current_code', 'initialLockingDate', 'nextLockingDate', 'createdAt'];
+        ?>
+
+        <div class="well">
+            <?=
+                PpdDetailView::widget([
+                    'model'      => $model,
+                    'attributes' => $systemDetailsAttributes,
+                ]) ?>
+        </div>
+
+    </div>
+
+<?php endif; ?>
+
